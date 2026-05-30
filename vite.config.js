8721b6edd43eb4d94e6d8a414f2654cbe3c48907
stevenh1607-boop <stevenh1607@gmail.com>
2026-05-30 21:04:16 +1000

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change 'iet-estimation-tool' to match your GitHub repository name exactly
 export default defineConfig({
   plugins: [react()],
-  base: '/iet-estimation-tool/',
+  base: '/iet-estimation-tool_demo/',
 })
